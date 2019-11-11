e2837aebda8419f40e14ef118c6abe8eb538862a
Dan Buettner <dan@danbuettner.net>
2019-11-11 16:53:55 -0600

    Use !!match rather than match? for Regexp in Rubies before 2.4

--- lib/omniauth/strategies/openid_connect.rb
+++ lib/omniauth/strategies/openid_connect.rb
@@ -135,7 +135,7 @@ module OmniAuth
       end
 
       def other_phase
-        if logout_path_pattern.match?(current_path)
+        if !!logout_path_pattern.match(current_path)
           options.issuer = issuer if options.issuer.to_s.empty?
           discover!
           return redirect(end_session_uri) if end_session_uri
